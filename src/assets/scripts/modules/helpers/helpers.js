b94ff4b46a348dc40133553dca16658161580e41
Dron2019 <55529637+Dron2019@users.noreply.github.com>
2021-02-17 14:40:05 +0200

    lint conflicts1

--- src/assets/scripts/modules/helpers/helpers.js
+++ src/assets/scripts/modules/helpers/helpers.js
@@ -29,6 +29,37 @@ export const eases = {
 };
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 export const langDetect = function () {
   if (window.location.pathname.match(/ru/)) {
     return 'ru';
