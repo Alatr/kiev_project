22bde342697102076bae53aff4b5ad412b1cc6f1
Dron2019 <mr.andrey@bigmir.net>
2021-02-22 13:33:13 +0200

    abour slider start

---
+++ src/assets/scripts/gulp-modules/about.js
@@ -0,0 +1,8 @@
+// eslint-disable-next-line no-undef
+const aboutSlider = new Swiper('.swiper-container', {
+  navigation: {
+    nextEl: '[data-next]',
+    prevEl: '[data-prev]',
+  },
+});
+aboutSlider.init();
