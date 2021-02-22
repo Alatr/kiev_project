019fa153c0805127083c9df8a371e479c7f03bce
Dron2019 <mr.andrey@bigmir.net>
2021-02-22 15:57:21 +0200

    about slider and start about adaptation

--- src/assets/scripts/gulp-modules/about.js
+++ src/assets/scripts/gulp-modules/about.js
@@ -1,8 +1,18 @@
+
 // eslint-disable-next-line no-undef
 const aboutSlider = new Swiper('.swiper-container', {
+  speed: 1000,
   navigation: {
     nextEl: '[data-next]',
     prevEl: '[data-prev]',
   },
+  effect: 'fade',
+  fadeEffect: {
+    crossFade: true,
+  },
 });
 aboutSlider.init();
+aboutSlider.on('beforeTransitionStart', () => {
+  // eslint-disable-next-line no-undef
+  gsap.fromTo('.swiper-container__bg', { x: '-120%' }, { x: '120%', duration: 1.5 });
+});
