a7c9c5a2af8f9a9ca809408f618645b2bf9f6fc1
Dron2019 <mr.andrey@bigmir.net>
2021-03-04 09:56:41 +0200

    values slider continuing develop

--- src/assets/scripts/gulp-modules/values.js
+++ src/assets/scripts/gulp-modules/values.js
@@ -6,26 +6,28 @@ const unactiveWidth = getComputedStyle(slider.querySelectorAll('.slide')[1]).wid
 const changeEasing = new BezierEasing(0.59, 0, 0.39, 1);
 // const changeEasing = new BezierEasing(.66,0,.39,.99);
 const changeDuration = 1.5;
+slider.style.setProperty('--js-transition', `${changeDuration}s`);
 slider.querySelectorAll('.slide').forEach((block) => {
   block.addEventListener('click', () => {
-    const active = document.querySelector('.active');
-    const activeWidth = getComputedStyle(active).width;
+    const activeSlide = document.querySelector('.active');
+    const activeWidth = getComputedStyle(activeSlide).width;
     const tl = gsap.timeline();
-
+    tl.set(slider, { pointerEvents: 'none' });
     tl.add(() => {
       block.classList.add('active');
     });
-    tl.fromTo(active,
+    tl.fromTo(activeSlide,
       { width: activeWidth },
       { width: unactiveWidth, ease: changeEasing, duration: changeDuration });
-    tl.fromTo(active.querySelector('img'), { scale: 1 }, { scale: 1.1, ease: changeEasing, duration: changeDuration }, '<');
+    tl.fromTo(activeSlide.querySelector('img'), { scale: 1 }, { scale: 1.1, ease: changeEasing, duration: changeDuration }, '<');
     tl.fromTo(block.querySelector('img'), { scale: 1.1 }, { scale: 1, ease: changeEasing, duration: changeDuration }, '<');
     tl.fromTo(block,
       { width: unactiveWidth },
       { width: activeWidth, ease: changeEasing, duration: changeDuration },
       '<');
     tl.add(() => {
-      active.classList.remove('active');
-    });
+      activeSlide.classList.remove('active');
+    }, '<');
+    tl.set(slider, { pointerEvents: 'all' });
   });
 });
