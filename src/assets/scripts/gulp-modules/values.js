3b8ed1d32eb4adbbe52e89266f6a936ac1857110
Dron2019 <mr.andrey@bigmir.net>
2021-03-04 14:37:21 +0200

    values slider adaptation

--- src/assets/scripts/gulp-modules/values.js
+++ src/assets/scripts/gulp-modules/values.js
@@ -1,36 +1,71 @@
 
 /* eslint-disable no-unused-expressions */
 /* eslint-disable no-undef */
+
 const slider = document.querySelector('[data-slider]');
 const unactiveWidth = getComputedStyle(slider.querySelectorAll('.slide')[1]).width;
-const changeEasing = new BezierEasing(0.79, 0.01, 0.32, 0.99);
-// const changeEasing = new BezierEasing(.66,0,.39,.99);
-const changeDuration = 1.25;
-slider.style.setProperty('--js-transition', `${changeDuration}s`);
+const unactiveHeight = getComputedStyle(slider.querySelectorAll('.slide')[1]).height;
+const transitionEasing = new BezierEasing(0.79, 0.01, 0.32, 0.99);
+const transitionDuration = document.documentElement.clientWidth > 575 ? 1.25 : 0.75;
+slider.style.setProperty('--js-transition', `${transitionDuration}s`);
 
-function animateSlideTransfer(block) {
-  const activeSlide = block.parentElement.querySelector('.active');
+function animateSlideTransfer(slide) {
+  const activeSlide = slide.parentElement.querySelector('.active');
   const activeWidth = getComputedStyle(activeSlide).width;
+  const tl = gsap.timeline();
+  tl.set(slider, { pointerEvents: 'none' });
+  tl.add(() => {
+    slide.classList.add('active');
+  });
+  tl.fromTo(activeSlide,
+    { width: activeWidth },
+    { width: unactiveWidth, ease: transitionEasing, duration: transitionDuration });
+  tl.fromTo(activeSlide.querySelectorAll('.slide__content>*'),
+    { autoAlpha: 1, y: 0 },
+    { autoAlpha: 0, y: -20, stagger: 0.1 }, '<');
+  tl.fromTo(activeSlide.querySelector('img'),
+    { scale: 1 },
+    { scale: 1.1, ease: transitionEasing, duration: transitionDuration }, '<');
+  tl.fromTo(slide.querySelector('img'),
+    { scale: 1.1 },
+    { scale: 1, ease: transitionEasing, duration: transitionDuration }, '<');
+  tl.fromTo(slide,
+    { width: unactiveWidth },
+    { width: activeWidth, ease: transitionEasing, duration: transitionDuration },
+    '<');
+  tl.add(() => {
+    activeSlide.classList.remove('active');
+  }, '<');
+  tl.fromTo(slide.querySelectorAll('.slide__content>*'),
+    { autoAlpha: 0, y: -20 },
+    { autoAlpha: 1, y: 0, stagger: 0.1 },
+    '-=0.5');
+  tl.set(slider, { pointerEvents: 'all' });
+}
+
+function animateSlideTransferHeight(block) {
+  const activeSlide = block.parentElement.querySelector('.active');
+  const activeHeight = getComputedStyle(activeSlide).height;
   const tl = gsap.timeline();
   tl.set(slider, { pointerEvents: 'none' });
   tl.add(() => {
     block.classList.add('active');
   });
   tl.fromTo(activeSlide,
-    { width: activeWidth },
-    { width: unactiveWidth, ease: changeEasing, duration: changeDuration });
+    { height: activeHeight },
+    { height: unactiveHeight, ease: transitionEasing, duration: transitionDuration });
   tl.fromTo(activeSlide.querySelectorAll('.slide__content>*'),
     { autoAlpha: 1, y: 0 },
     { autoAlpha: 0, y: -20, stagger: 0.1 }, '<');
   tl.fromTo(activeSlide.querySelector('img'),
     { scale: 1 },
-    { scale: 1.1, ease: changeEasing, duration: changeDuration }, '<');
+    { scale: 1.1, ease: transitionEasing, duration: transitionDuration }, '<');
   tl.fromTo(block.querySelector('img'),
     { scale: 1.1 },
-    { scale: 1, ease: changeEasing, duration: changeDuration }, '<');
+    { scale: 1, ease: transitionEasing, duration: transitionDuration }, '<');
   tl.fromTo(block,
-    { width: unactiveWidth },
-    { width: activeWidth, ease: changeEasing, duration: changeDuration },
+    { height: unactiveHeight },
+    { height: activeHeight, ease: transitionEasing, duration: transitionDuration },
     '<');
   tl.add(() => {
     activeSlide.classList.remove('active');
@@ -39,12 +74,14 @@ function animateSlideTransfer(block) {
     { autoAlpha: 0, y: -20 },
     { autoAlpha: 1, y: 0, stagger: 0.1 },
     '-=0.5');
-
   tl.set(slider, { pointerEvents: 'all' });
 }
-function configSlideBehavior(block) {
-  block.addEventListener('click', () => {
-    animateSlideTransfer(block);
+function configSlideBehavior(slide) {
+  slide.addEventListener('click', () => {
+    (document.documentElement.clientWidth > 575)
+      ? animateSlideTransfer(slide)
+      : animateSlideTransferHeight(slide);
+
     clearInterval(slider.slideAutoPlay);
   });
 }
@@ -53,7 +90,18 @@ slider.querySelectorAll('.slide').forEach(configSlideBehavior);
 
 let indexSl = 1;
 slider.slideAutoPlay = setInterval(() => {
-  animateSlideTransfer(slider.querySelectorAll('.slide')[indexSl]);
+  (document.documentElement.clientWidth > 575)
+    ? animateSlideTransfer(slider.querySelectorAll('.slide')[indexSl])
+    : animateSlideTransferHeight(slider.querySelectorAll('.slide')[indexSl]);
   // console.log(indexSl);
   indexSl === slider.querySelectorAll('.slide').length - 1 ? indexSl = 0 : indexSl += 1;
 }, 5000);
+
+
+/* ПОдмена картинок на мобильной версии если такие есть */
+if (document.documentElement.clientWidth < 576) {
+  document.querySelectorAll('[data-mob]').forEach((mobImg) => {
+    const img = mobImg;
+    img.src = img.dataset.mob;
+  });
+}
